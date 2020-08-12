67c4289fc7387f3b8f06041281c2921698362c67
Ayuha Koyanagi <ayuhakoyanagi@AyuhanoMacBook-Air.local>
2020-08-12 14:33:13 +0900

    second commit

--- script.js
+++ script.js
@@ -3,12 +3,5 @@ var app = new Vue({
   el: '#app',
   data: {
   	message: 'Hello Vue.js!',
-    count: 0,
-    user: {
-    	lastName: 'Yamada',
-      firstName: 'Taro',
-      prefecture: 'Tokyo'
-    },
-    colors: ['Red', 'Green', 'Blue']
   }
 })
